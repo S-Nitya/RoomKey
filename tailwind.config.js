d87336e1d0cc232292818c56d17ca6ff26bc6c74
siddhi-ms <smsushir2023@gmail.com>
2025-09-19 09:21:00 +0530

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx}",   // ✅ include .js and .jsx files
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
